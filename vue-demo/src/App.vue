2ca8d33108d3e26edd49be08dc641dadd56660a5
ayedaren8 <1783580635@qq.com>
2020-05-11 17:29:25 +0800

    nothing

---
+++ vue-demo/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <HelloWorld msg="nothing"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+
+</style>
